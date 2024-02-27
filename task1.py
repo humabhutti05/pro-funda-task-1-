c62cea7a1001caf8d4188d0491f6d07061d20d4a
Huma Muhammad Yousaf <126263235+humabhutti05@users.noreply.github.com>
2024-02-27 06:06:30 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,40 @@
+print("hello world")
+
+# Pythagorean Theorem:
+a=7
+b=9
+
+c=(a**2 + b**2)**0.5
+
+print("Pythagorean Theorem :",c)
+
+# Area of triangle:
+length=5
+width=7
+area=length*width
+print("Area of triangle:",area)
+
+# Area of circle
+r= 15
+pi=3.142
+
+A= pi*(r**2)
+print("Area of circle:", A)
+
+# Volume of a cylinder
+r=4
+h=7
+
+pi=3.142
+V= pi* (r**2)* h
+print("Volume of a cylinder:", V)
+
+# linear equation
+# y=mx +c
+m=5
+x=4
+c= 8
+
+y = 5*4 + 8
+
+print("Value of Y is:" ,y)
